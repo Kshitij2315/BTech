7a46ded38fac9b226b81c5c2c8ea238d12d41ec1
Kshitij2315 <come2kshitij@gmail.com>
2023-04-30 20:18:12 +0530

    Simple Interest

---
+++ GradeOfStudent.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+WAP to print Area of a Circle
+
+******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float radius=0.0;
+    float area=0.0;
+    float PIE=3.14;
+
+    printf("Enter radius of circle: ");
+    scanf("%f", &radius);
+
+    area = PIE * radius * radius;
+
+    
+    printf("Area of circle:%f",area);
+
+    return 0;
+}
