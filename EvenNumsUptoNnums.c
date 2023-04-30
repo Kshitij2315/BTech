3b07b39b772a1f97c6f7bde32d3784d0a1f26e87
Kshitij2315 <come2kshitij@gmail.com>
2023-04-30 21:36:16 +0530

    Enen Numbers Upto N Numbers

---
+++ EvenNumsUptoNnums.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+WAP to print only Even Number Upto N Nums
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   int i=0;
+   int N=0;
+   
+   printf("Till: ");
+   scanf("%d",&N);
+   
+   printf("The Even Numbers are:");
+   
+       while (i<=N)
+       {
+           printf("%d\n",i);
+           
+           i = i + 1;
+       }
+
+    return 0;
+}
