4dc503468ebf9a3f2e3ee09a9f0442bea53dff20
Kshitij2315 <come2kshitij@gmail.com>
2023-05-16 23:16:23 +0530

    NxM Matrix (2D Array)

---
+++ C_Lang/NxMMatrix.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+WAP to Read and Print NxM Matrix
+
+******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int arr[100][100],i,j,N,M;
+    
+    printf("Enter How many Rows = ");
+    scanf("%d",&N);
+    
+    printf("Enter How many columns = ");
+    scanf("%d",&M);
+    
+     for(i=0; i<N; i++)
+     {
+        for(j=0; j<M; j++)
+        {
+             printf("Enter Element arr[%d][%d]: ",i,j);
+             scanf("%d",&arr[i][j]);
+        }
+     }
+     
+     for(i=0; i<N; i++)
+     {
+         for(j=0; j<M; j++)
+         {
+             printf("\narr[%d][%d] = %d",i,j,arr[i][j]);
+         }
+     }
+   
+    return 0;
+}
+
+
