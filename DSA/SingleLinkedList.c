31b732e152c08faeb77eac860ee4cc95ea33f35e
Kshitij Bhardwaj <come2kshitij@gmail.com>
2024-09-14 19:06:08 +0530

    Adding insertAfter func.

--- DSA/SingleLinkedList.c
+++ DSA/SingleLinkedList.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct node *u;
+
 struct node {
     int data;
     struct node *next;
@@ -17,14 +19,14 @@ struct node *start = NULL;
 struct node *createNode(){
     struct node *newnode;
     newnode = (struct node *) malloc(sizeof(struct node));
-    newnode -> next = ' ';
+    newnode -> next = '\0';
 } 
 
 void createList(){
     int data=0;
     struct node *newnode;
     
-        printf("nEnter data: n"); 
+        printf("\nEnter data: "); 
         scanf("%d",&data);
         
         newnode=createNode();
@@ -32,7 +34,8 @@ void createList(){
         
         if(start == NULL ){ // only for first time
            start = newnode; 
-        }else{
+        }else
+        {
             struct node *temp;
             temp = start;
             while(temp -> next != NULL) 
@@ -41,8 +44,6 @@ void createList(){
            
             temp -> next = newnode; //linking
         }
-        
-    
 }
 
 void display(){
@@ -57,22 +58,84 @@ void display(){
     
 }
 
+struct node *search(int find)
+{
+    //int find;
+    struct node *temp;
+    int found = 0;
+    
+    /*printf("\nEnter Data to Search for: ");
+    scanf("%d",&find);*/
+    
+    temp = start;
+    
+    while( temp != NULL)
+    {
+        if(temp -> data == find)
+        {
+            found = 1;
+            break;
+        }
+        else
+        {
+            temp = temp -> next;
+        }   
+    }
+    
+    /*if(found == 1)
+    {
+        printf("\nData is present in the List.\n\n");
+    }
+    else
+    {
+        printf("\nData is not present in the List.\n\n");
+    }*/
+    
+   return temp; 
+} 
+
+void insertAfter()
+{
+    int after, data;
+    struct node *temp, *newnode;
+    
+    printf("Insert After: ");
+    scanf("%d",&after);
+    
+    temp = search(after);
+    
+    newnode = createNode();
+    
+    printf("\nEnter data: "); 
+    scanf("%d",&data);
+    
+    newnode -> data = data;
+    
+    newnode -> next = temp -> next;
+    
+    temp -> next = newnode;
+}
+
 int main()
 {
     int ans=1;
-    while (ans >0){
-        printf ("n 1. for createList: ");
-        printf("n 2. for display: ");
-        printf("nEnter your choice");
+    while (ans >0)
+    {
+        printf("\n 1. To CreateList: ");
+        printf("\n 2. To Search: ");
+        printf("\n 3. To Display: ");
+        printf("\n 4. To Insert After: ");
+        printf("\nEnter your choice: ");
         scanf("%d",&ans);
         
-        switch(ans){
+        switch(ans)
+        {
         case 1: createList(); break;
-        case 2: display(); break;
-
-
-        
+        //case 2: search(); break;
+        case 3: display(); break;
+        case 4: insertAfter(); break;
         }
     }
+    
     return 0;
 }
