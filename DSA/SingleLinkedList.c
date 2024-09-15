342541deb6dffad751c5757853084ad8029f6b4d
Kshitij Bhardwaj <come2kshitij@gmail.com>
2024-09-15 18:46:12 +0530

    Adding insertBefore and Delete func.

--- DSA/SingleLinkedList.c
+++ DSA/SingleLinkedList.c
@@ -58,14 +58,14 @@ void display(){
     
 }
 
-struct node *search(int find)
+void search()
 {
-    //int find;
+    int find;
     struct node *temp;
     int found = 0;
     
-    /*printf("\nEnter Data to Search for: ");
-    scanf("%d",&find);*/
+    printf("\nEnter Data to Search for: ");
+    scanf("%d",&find);
     
     temp = start;
     
@@ -82,16 +82,14 @@ struct node *search(int find)
         }   
     }
     
-    /*if(found == 1)
+    if(found == 1)
     {
         printf("\nData is present in the List.\n\n");
     }
     else
     {
         printf("\nData is not present in the List.\n\n");
-    }*/
-    
-   return temp; 
+    }
 } 
 
 void insertAfter()
@@ -102,11 +100,23 @@ void insertAfter()
     printf("Insert After: ");
     scanf("%d",&after);
     
-    temp = search(after);
+    temp = start;
+    
+    while( temp != NULL)
+    {
+        if(temp -> data == after)
+        {
+            break;
+        }
+        else
+        {
+            temp = temp -> next;
+        }   
+    }
     
     newnode = createNode();
     
-    printf("\nEnter data: "); 
+    printf("\nEnter data to insertAfter %d: ",after); 
     scanf("%d",&data);
     
     newnode -> data = data;
@@ -116,24 +126,117 @@ void insertAfter()
     temp -> next = newnode;
 }
 
+void insertBefore()
+{
+    int before, data;
+    struct node *temp, *newnode,*prev;
+    
+    printf("Insert Before: ");
+    scanf("%d",&before);
+    
+    printf("\nEnter data to insertBefore %d: ",before);
+    scanf("%d", &data);
+    
+    newnode = createNode();
+    
+    newnode->data = data;
+    
+    prev = NULL;
+    temp = start;
+    
+    if(before != start -> data)
+    {
+        while( temp != NULL)
+        {
+            if(temp -> data == before)
+            {
+                break;
+            }
+            else
+            {
+                prev = temp;
+                temp = temp -> next;
+            }   
+        } 
+        
+        prev->next = newnode;
+    }
+    else
+    {
+        newnode -> next = start;
+        start = newnode;
+    }
+    
+    newnode->next = temp;
+    
+   
+}
+
+void delete()
+{
+    int data;
+    struct node *temp, *newnode,*prev;
+    
+    printf("Enter data to be deleted: ");
+    scanf("%d",&data);
+    
+    prev = NULL;
+    temp = start;
+    
+    if(data != start -> data)
+    {
+        while( temp != NULL)
+        {
+            if(temp -> data == data)
+            {
+                break;
+            }
+            else
+            {
+                prev = temp;
+                temp = temp -> next;
+            }
+        }
+        
+        prev->next = temp->next;
+    }
+    else
+    {
+        start = start -> next;
+    }
+      
+}
+
 int main()
 {
     int ans=1;
-    while (ans >0)
+    while (ans > 0)
     {
+        printf("\n--------------------------------");
         printf("\n 1. To CreateList: ");
         printf("\n 2. To Search: ");
         printf("\n 3. To Display: ");
         printf("\n 4. To Insert After: ");
-        printf("\nEnter your choice: ");
+        printf("\n 5. To Insert Before: ");
+        printf("\n 6. To Delete: ");
+        printf("\n\nEnter your choice: ");
         scanf("%d",&ans);
+        printf("--------------------------------\n\n");
         
         switch(ans)
         {
-        case 1: createList(); break;
-        //case 2: search(); break;
-        case 3: display(); break;
-        case 4: insertAfter(); break;
+        case 1: createList(); 
+                break;
+        case 2: search(); 
+                break;
+        case 3: display(); 
+                break;
+        case 4: insertAfter(); 
+                break; 
+        case 5: insertBefore(); 
+                break;
+        case 6: delete(); 
+                break;
         }
     }
     
