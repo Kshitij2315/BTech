4c5a7230f93f2c4a5fb67263d4ee1c0212c86789
Kshitij Bhardwaj <come2kshitij@gmail.com>
2025-02-02 08:49:31 +0530

    For loop and range()

--- Python/Loop.py
+++ Python/Loop.py
@@ -1,8 +1,21 @@
+#list = [1, 3, 5, 7]
+tup=(1, 3, 5, 7)
 
-i = 0
-while i <= 5:
-    if i == 3:
-        i += 1
-        continue
-    print(i)
-    i += 1
+"""for i in tup:   #list:
+    print(i)"""
+
+#range():
+"""for i in range(4):                 #range(start?, stop, step?)
+    print(i)"""
+
+'''for i in range(2, 5):
+    print(i)'''
+
+'''for i in range(2, 20, 2):
+    print(i)'''
+
+#pass:
+for i in tup:   #list:
+    pass                        #empty
+
+print("hello")
